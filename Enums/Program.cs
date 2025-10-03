91e776859f44868032f3998e0c3abe5945ae814e
Caio Fernando Schneider Silva <caio.fernando.silva@outlook.com>
2025-10-03 11:39:24 -0300

    Adicionar arquivos de projeto.

---
+++ Enums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    internal class Program
+    {
+        enum NotaDeReal_e //enums devem ser declarados fora do metodo main
+        {
+            NotaDe2 = 2, //separando os valores com virgula, o ultimo nao precisa de nada
+            NotaDe5 = 5, // apenas numeros inteiros
+            NotaDe10 = 10
+        }
+        enum NotaDaProva
+        {
+            Nota0 = 0,
+            Nota1,
+            Nota2,      //desta forma ele faz automaticamente um incremento de 1 em 1
+            Nota3,
+            Nota4
+        }
+        static void Main(string[] args)
+        {
+            NotaDeReal_e minhaNota = NotaDeReal_e.NotaDe10; //para usar o enum, declare uma variavel do tipo do enum e atribua um dos valores
+            Console.WriteLine(minhaNota + " vale " + Convert.ToUInt32(minhaNota)); 
+            NotaDaProva notaProva = NotaDaProva.Nota3; 
+            Console.WriteLine(notaProva + " vale " + Convert.ToUInt32(notaProva));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Enums/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Enums")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Enums")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("059ba183-2e47-48e0-83d9-d2faf2a1dc5f")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
